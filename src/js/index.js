932e2a4f56d1ddc42dba4919cfdc76daa5a3e465
Hel <helenahrubesova80@gmail.com>
2019-10-04 10:38:30 +0200

    fixing eslint

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,9 @@
 document.addEventListener('DOMContentLoaded', () => {
   const stageClick = document.querySelector('.container');
   stageClick.addEventListener('click', () => {
-    const pacman = document.querySelector('#pacman')
+    const pacman = document.querySelector('#pacman');
     if (pacman.style.backgroundPositionX === '100%') {
       pacman.style.backgroundPositionX = '0%';
-    } else pacman.style.backgroundPositionX = '100%'
-    
+    } else pacman.style.backgroundPositionX = '100%';
   });
-
 });
